a02f2fbc7929ebe32e1c37987572e87824a5b9d0
CaptainSara <sara.iqbal@hotmail.se>
2023-05-21 00:08:09 +0200

    working version

--- client/src/components/Announcement.jsx
+++ client/src/components/Announcement.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useQuery, gql } from '@apollo/client'
 //import { Link } from 'react-router-dom'
 
-const ANNOUN = gql`
+const ANNOUNCE = gql`
 query get {
   announcements {
     data {
@@ -20,7 +20,7 @@ query get {
 
 export default function Announcement() {
 
-  const {data, loading, error} = useQuery(ANNOUN)
+  const {data, loading, error} = useQuery(ANNOUNCE)
   if(loading) return "Loading..."
   if(error) return "Error"
 
@@ -29,7 +29,7 @@ export default function Announcement() {
         {data.announcements.data.map((value) => (
           <div key={value.id} className="announce-card">
             <p className='published-at'>{value.attributes.publishedAt.slice(0, 10)} {value.attributes.publishedAt.slice(11, 16)}</p>
-            <h3>{value.attributes.title}</h3>
+            <h3><b>{value.attributes.title}</b></h3>
             <p>{value.attributes.message}</p>
             {/* <p>{value.attributes.message.substring(0, 200)}... <Link to={`/announcements/${value.id}`}>Read more</Link></p> */}
             <p>Published By: {value.attributes.publishedBy}</p>
